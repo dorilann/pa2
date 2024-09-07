ef0cce78c77f84a9edd18b140a320cf997b04b68
Alexandr Caraman <pepegunj@gmail.com>
2024-09-07 03:32:30 +0300

    update workflow

---
+++ ApiGateway/Controllers/Health.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Health : ControllerBase
+    {
+        [HttpGet("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Is Alive");
+        }
+    }
+}
---
+++ WeatherServiceTest/Controllers/Health.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WeatherService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Health : ControllerBase
+    {
+        [HttpGet("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Is Alive");
+        }
+    }
+}
